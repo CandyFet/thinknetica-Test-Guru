983944785b257957ef66e801c419a715fab73650
root <inorect.a.z@gmail.com>
2019-07-31 23:39:06 +0300

    Additions due Max comments

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   def self.names_by_category(category_title)
-    order(title: :desc).joins("INNER JOIN categories ON categories.id = tests.category_id").where(categories: { title: category_title })
+    titles = []
+    order(title: :desc).joins("INNER JOIN categories ON categories.id = tests.category_id").where(categories: { title: category_title }).each do |test|
+      titles << test.title
+    end
+    titles
   end
 end
