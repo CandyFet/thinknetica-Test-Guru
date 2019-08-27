5143ded9d44c9667e1b91af52219b939597c9175
CandyFet <inorect.a.z@gmail.com>
2019-08-27 13:20:26 +0300

    Refactoring associations

--- db/migrate/20190823113037_add_creator_to_test.rb
+++ db/migrate/20190823113037_add_creator_to_test.rb
@@ -1,5 +1,5 @@
 class AddCreatorToTest < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tests, :creator, foreign_key: true
+    add_reference :tests, :creator, foreign_key: { to_table: :tests }
   end
 end
