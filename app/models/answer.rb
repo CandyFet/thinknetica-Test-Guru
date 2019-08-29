da90eb7ea5ca9c55adece1b0d3c8fe846164b325
CandyFet <inorect.a.z@gmail.com>
2019-08-29 10:46:48 +0300

    init

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  validate :validates_amount_of_questions
+
+  scope :correct, -> { where(correct: true) }
+
+
+  private
+
+  def validates_amount_of_questions
+    errors.add(:body) unless question.answers.size.in?(1..4)
+  end
+
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
@@ -5,7 +7,19 @@ class Test < ApplicationRecord
   has_many :test_users, dependent: :destroy
   has_many :users, through: :test_users
 
-  def self.names_by_category(category_title)
-    order(title: :desc).joins(:category).where(categories: { title: category_title }).pluck(:title)
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+  validate :validates_uniqueness_of_attributes, :title, :level
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :names_by_category, ->(category) { order(title: :desc).joins(:category).where(categories: { title: category }).pluck(:title) }
+  scope :by_level, ->(level) { where(tests: { level: level }) }
+
+  private
+
+  def validates_uniqueness_of_attributes(*attrs)
+    attrs.each { |attr| validates attr, uniqueness: true }
   end
 end
