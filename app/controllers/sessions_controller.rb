f3ff4bf9c5f03857d9143d36a4c0064577e35dff
CandyFet <inorect.a.z@gmail.com>
2019-12-19 10:03:47 +0300

    refactored

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session[:last_page] || root_path
-      session[:last_page] = nil
+      redirect_to cookies.delete[:last_page] || root_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your email and password please.'
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to session[:last_page] || root_path
-      session[:last_page] = nil
+      redirect_to cookies.delete[:last_page] || root_path
     else
       render :new
     end
